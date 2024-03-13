1a44db39d3f55546d06fb8ae841dbaa8821dd9c6
nah-coder <huyanhnguyen1211@gmail.com>
2024-03-13 23:40:08 +0700

    huy nhe

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("")
+    public List<Student> findAll(){
+        return studentService.findAll();
+    }
+}
---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "clazz")
+    private String clazz;
+
+}
